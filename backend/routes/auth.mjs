42f638371f894316aeb2cc35e48ad75da6c33a7b
Bruce <brucengobeni2005@gmail.com>
2025-02-06 14:05:53 +0200

    Implemented password hashing using bcrypt and handling errors using error handling middleware

--- backend/routes/auth.mjs
+++ backend/routes/auth.mjs
@@ -1,37 +1,56 @@
 import { Router as router } from "express";
 import User from "../models/user.mjs"
+import bcrypt from 'bcryptjs';
 
 
 router.post("/register", async (req, res, next) => {
 
-    const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    const existingUser = await User.findOne({email});
-    if(existingUser){
-        return res.status(400).json({message: "Email already in use"})
+        // check if email is already in use
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            const error = new Error("Email already in use");
+            error.status = 400;
+            return next(error);
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        // Create the new user with hashed password
+        const newUser = new User({email, password: hashedPassword});
+
+        // Save user to the database
+        await newUser.save();
+
+        return res.status(201).json({message: "User successfully created", user: newUser});
+
+
+    } catch (error) {
+        next(error);
     }
-
-
-    const newUser = new User({email, password});
-    await newUser.save();
-
-    return res.status(201).json({message: "User successfully created", user: newUser})
-
 
 });
 
 
 router.post("/login", async (req, res, next) => {
 
-    const user = await User.findOne({email: req.body.email});
+    const user = await User.findOne({ email: req.body.email });
 
-    if(!user){
-        return res.status(404).json({message: "User not found"});
+    if (!user) {
+        return res.status(404).json({ message: "User not found" });
     }
 
-    if(user.password != req.res.password){
-        return res.status(401).json({message: "Wrong password"});
+    if (user.password != req.res.password) {
+        return res.status(401).json({ message: "Wrong password" });
     }
 
-    res.status(200).json({user, message: "Successfully logged in"})
+    res.status(200).json({ user, message: "Successfully logged in" })
 })
+
+
+
+
+export default router;
