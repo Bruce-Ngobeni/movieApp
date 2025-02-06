5356b8be26e33d18ec813d780c15aa58647fde00
Bruce <brucengobeni2005@gmail.com>
2025-02-06 11:53:23 +0200

    Modified register route in auth.mjs file to account for existing emails

--- backend/routes/auth.mjs
+++ backend/routes/auth.mjs
@@ -1,1 +1,37 @@
+import { Router as router } from "express";
+import User from "../models/user.mjs"
 
+
+router.post("/register", async (req, res, next) => {
+
+    const { email, password } = req.body;
+
+    const existingUser = await User.findOne({email});
+    if(existingUser){
+        return res.status(400).json({message: "Email already in use"})
+    }
+
+
+    const newUser = new User({email, password});
+    await newUser.save();
+
+    return res.status(201).json({message: "User successfully created", user: newUser})
+
+
+});
+
+
+router.post("/login", async (req, res, next) => {
+
+    const user = await User.findOne({email: req.body.email});
+
+    if(!user){
+        return res.status(404).json({message: "User not found"});
+    }
+
+    if(user.password != req.res.password){
+        return res.status(401).json({message: "Wrong password"});
+    }
+
+    res.status(200).json({user, message: "Successfully logged in"})
+})
